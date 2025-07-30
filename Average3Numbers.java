e6e87bf52e928220e9b7adff48790e0d5e1fe9f2
harjot <harjot362.be22@chitkara.edu.in>
2025-07-30 11:07:28 +0530

    average of 3 numbers

---
+++ Average3Numbers.java
@@ -0,0 +1,20 @@
+// AverageThreeNumbers.java
+import java.util.Scanner;
+
+public class AverageThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1 = scanner.nextDouble();
+
+        double num2 = scanner.nextDouble();
+
+        double num3 = scanner.nextDouble();
+
+        
+        double average = (num1 + num2 + num3) / 3;
+
+        
+        System.out.println("The average is: " + average);
+    }
+}
