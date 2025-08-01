bdfaf4499982c76998a9b2f078401ad36179cbbe
harjot <harjot362.be22@chitkara.edu.in>
2025-08-01 22:01:35 +0530

    commi-1t

---
+++ extra/PalindromeCheck.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        input = input.toLowerCase();
+        
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
